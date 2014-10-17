f4db1cd0bae6f0222f3535ce183064bbbd39f4c7
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-10-17 08:33:27 +0100

    Fix default spec.

--- bootstrap/spec/recipes/default_spec.rb
+++ bootstrap/spec/recipes/default_spec.rb
@@ -1,15 +1,22 @@
 require_relative '../spec_helper'
 
-describe 'bootstrap::setup' do
+describe 'bootstrap::default' do
   let(:chef_run) do
     ChefSpec::Runner.new(
-      cookbook_path: ['site-cookbooks', 'cookbooks'],
+      cookbook_path: ['../../../tmp/cookbooks'],
       platform:      'centos',
       version:       '6.5'
-    ).converge('bootstrap::setup')
+    ).converge('bootstrap::default')
+  end
+
+  before do
+    ENV['PATTERN_NAME'] = 'test_pattern'
+    ENV['PATTERN_URL'] = 'test_url'
+    ENV['PATTERN_REVISION'] = 'test_revision'
+    ENV['SERF_TAG_ROLE'] = 'web,ap,db'
   end
 
   it 'include setup' do
-    expect(chef_run).to include_recipe 'bootstrap::setup'
+    expect(chef_run).to include_recipe('bootstrap::setup')
   end
 end
