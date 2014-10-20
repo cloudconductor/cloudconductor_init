64b13c3ff18bcb34120e5eb2f03bc53fd837884b
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-10-20 06:54:03 +0100

    Fix roles literal.

--- spec/lib/cloud_conductor_utils/serf_spec.rb
+++ spec/lib/cloud_conductor_utils/serf_spec.rb
@@ -16,22 +16,24 @@ require_relative '../../../lib/cloud_conductor_utils/serf'
 
 module CloudConductorUtils
   describe Serf do
-    it 'host_info' do
-      hostname = 'dummyhost'
-      dummy_info = 'dummyhost 192.168.0.1:7946 alive role=web,ap,db'
-      allow(CloudConductorUtils::Serf).to receive(:`).with('hostname').and_return(hostname)
-      allow(CloudConductorUtils::Serf).to receive(:`).with('serf members -name="^dummyhost$"').and_return(dummy_info)
-      allow(CloudConductorUtils::Pattern).to receive(:platform_pattern_name).and_return('platform1')
-      expected = [
-        'dummyhost',
-        {
-          roles: %w('web', 'ap', 'db'),
-          pattern: 'platform1',
-          private_ip: '192.168.0.1'
-        }
-      ]
-      result = Serf.host_info
-      expect(result.to_s).to eq(expected.to_s)
+    describe 'host_info' do
+      it 'formats and returns information of host' do
+        hostname = 'dummyhost'
+        dummy_info = 'dummyhost 192.168.0.1:7946 alive role=web,ap,db'
+        allow(CloudConductorUtils::Serf).to receive(:`).with('hostname').and_return(hostname)
+        allow(CloudConductorUtils::Serf).to receive(:`).with('serf members -name="^dummyhost$"').and_return(dummy_info)
+        allow(CloudConductorUtils::Pattern).to receive(:platform_pattern_name).and_return('platform1')
+        expected = [
+          'dummyhost',
+          {
+            roles: %w(web ap db),
+            pattern: 'platform1',
+            private_ip: '192.168.0.1'
+          }
+        ]
+        result = Serf.host_info
+        expect(result.to_s).to eq(expected.to_s)
+      end
     end
   end
 end
