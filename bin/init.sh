f346e9e1c17a3815de855952f8750cafab057e72
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-10-31 01:39:55 +0000

    Modify cloud_conductor_utils branch setting to master.

--- bin/init.sh
+++ bin/init.sh
@@ -23,7 +23,7 @@ cd ${TMP_DIR}
 log_info "install cloud_conductor_utils."
 git clone https://github.com/cloudconductor/cloud_conductor_utils.git
 cd cloud_conductor_utils
-git checkout develop
+git checkout master
 rake build
 cd pkg
 gem install ./*.gem
