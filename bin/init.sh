53026b7226ae0a9dac0d86cca5a26ab1ee24022e
Masaya Muraki <muraki.masaya@tis.co.jp>
2014-12-19 08:26:13 +0000

    Checkout feature/consul_security temporarily.

--- bin/init.sh
+++ bin/init.sh
@@ -23,6 +23,7 @@ cd ${TMP_DIR}
 log_info "install cloud_conductor_utils."
 git clone https://github.com/cloudconductor/cloud_conductor_utils.git
 cd cloud_conductor_utils
+git checkout feature/consul_security
 rake build
 cd pkg
 gem install ./*.gem
