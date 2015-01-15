2c39fb9c0850021e1936692972bb1dae43b9509a
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-01-15 01:50:12 +0000

    Disable encryption, ACL in case of missing CONSUL_SECURITY_KEY.

--- bootstrap/attributes/default.rb
+++ bootstrap/attributes/default.rb
@@ -15,18 +15,25 @@ default['consul']['service_mode'] = 'server'
 default['consul']['service_user'] = 'root'
 default['consul']['service_group'] = 'root'
 default['consul']['bind_addr'] = '0.0.0.0'
-default['consul']['encrypt'] = ENV['CONSUL_SECURITY_KEY']
 default['consul']['ports'] = node['consul']['ports'].merge({'https' => 8501})
 default['consul']['datacenter'] = 'dc1'
 default['consul']['extra_params'] = {
   'ca_file' => '/etc/pki/tls/cert.pem',
   'cert_file' => node['cloudconductor']['consul']['ssl_cert'],
-  'key_file' => node['cloudconductor']['consul']['ssl_key'],
-  'acl_datacenter' => node['consul']['datacenter'],
-  'acl_default_policy' => 'deny',
-  'acl_master_token' => ENV['CONSUL_SECURITY_KEY'],
-  'acl_token' => 'nothing'
+  'key_file' => node['cloudconductor']['consul']['ssl_key']
 }
+
+unless "#{ENV['CONSUL_SECURITY_KEY']}".empty? then
+  default['consul']['encrypt'] = ENV['CONSUL_SECURITY_KEY']
+  default['consul']['extra_params'].merge!(
+    {
+      'acl_datacenter' => node['consul']['datacenter'],
+      'acl_default_policy' => 'deny',
+      'acl_master_token' => ENV['CONSUL_SECURITY_KEY'],
+      'acl_token' => 'nothing'
+    }
+  )
+end
 
 # set pattern attributes
 default['cloudconductor']['pattern_name'] = ENV['PATTERN_NAME']
